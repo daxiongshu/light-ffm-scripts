b4b71ddd02b5a6a12a13e52536cf9b94270b8242
daxiongshu <aixueer4ever@gmail.com>
2017-05-17 15:06:08 -0400

    write feature map for outbrain

--- write_feamap.py
+++ write_feamap.py
@@ -1,7 +1,14 @@
-num = 18 
+import sys
+at = int(sys.argv[1])
+num = 29 
 fo = open('feamap.txt','w')
 for i in range(num):
-    line = ' '.join([str(j) for j in range(num)])
+    if i== at:
+	line = ' '.join(['-1' for j in range(num)])
+    elif i<13:
+   	line = ' '.join(['-1' for j in range(13)]+[str(j-13) for j in range(13,25)]+['12' for j in range(25,num)])
+    else:
+	line = ' '.join([str(j) for j in range(13)]+['-1' for j in range(13,num)])
     fo.write('%d: %s\n'%(i,line))
 fo.close()
 	
